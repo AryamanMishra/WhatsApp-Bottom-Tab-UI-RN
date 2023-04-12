72d0d6a0a79a9b9c190b30d0d9a7eaa183f449e0
modest_aryaman09 <61847192+AryamanMishra@users.noreply.github.com>
2023-04-12 12:08:39 +0530

    updated calls page

--- tabs/Calls.js
+++ tabs/Calls.js
@@ -2,27 +2,38 @@ import React from 'react'
 import { Text, View,StyleSheet, ScrollView } from 'react-native'
 import CreateCallLink from '../components/Calls/CreateCallLink'
 import RecentCall from '../components/Calls/RecentCall'
+import AddCallIcon from 'react-native-vector-icons/MaterialIcons'
 
 
 const Calls = ()=> {
     return (
-        <ScrollView style={styles.mainCalls}>
-            <CreateCallLink />
-            <View style={styles.recentCallsSection}>
-                <Text style={styles.subheadingText}>Recent</Text>
-                <View style={styles.recentCalls}>
-                        <RecentCall />
-                        <RecentCall />
-                        <RecentCall />
-                        <RecentCall />
-                        <RecentCall />
-                        <RecentCall />
-                        <RecentCall />
-                        <RecentCall />
-                        <RecentCall />      
+        <View style={styles.mainCalls}>
+            <ScrollView>
+                <CreateCallLink />
+                <View style={styles.recentCallsSection}>
+                    <Text style={styles.subheadingText}>Recent</Text>
+                    <View style={styles.recentCalls}>
+                            <RecentCall />
+                            <RecentCall />
+                            <RecentCall />
+                            <RecentCall />
+                            <RecentCall />
+                            <RecentCall />
+                            <RecentCall />
+                            <RecentCall />
+                            <RecentCall />      
+                    </View>
                 </View>
+            </ScrollView>
+            <View style={styles.iconsView}>
+                <AddCallIcon 
+                    color='white'
+                    name='add-call'
+                    size={24}
+                    style={styles.addCallIcon}
+                />
             </View>
-        </ScrollView>
+        </View>
     )
 }
 
@@ -43,6 +54,19 @@ const styles = StyleSheet.create({
     recentCalls : {
         gap:8
     }, 
+    iconsView : {
+        position:'absolute',
+        top:'88.8%',
+        right:'4%',
+        gap:16,
+        alignItems:'center'
+    },
+    addCallIcon : {
+        backgroundColor:'rgb(40,160,90)',  
+        paddingVertical:18.75,
+        paddingHorizontal:18.75,
+        borderRadius:50
+    }
 })
 
 
